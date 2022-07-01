75812fccc86afe744c1363c6b520e156047f1115
Sober_Clever <Sober_Clever@mail.ustc.edu.cn>
2022-07-01 12:47:21 +0800

    Modify a

--- WebContent/src/main/java/userManagement/GetFileList.java
+++ WebContent/src/main/java/userManagement/GetFileList.java
@@ -82,7 +82,7 @@ public class GetFileList extends ActionSupport{
 			html = html + 
 			"<tr class=\"file_list_go\">"+
 				"<td> </td>"+
-				(fileArray[i].isFolder()?"<td> <label><input style=\"height: 20px; width:20px; margin: 0 auto; display: inline; float:left;\" type=\"checkbox\"> &emsp;&emsp;</label><i class=\"ion-android-folder\"> " + fileArray[i].getFileName()+"</i></td>":"<td> <label><input style=\"height: 20px; width:20px; margin: 0 auto; display: inline; float:left;\" type=\"checkbox\">&emsp;&emsp;</label> <i input class=\"glyphicon glyphicon-file\"> " + fileArray[i].getFileName()+"</i></td>") +
+				(fileArray[i].isFolder()?"<td> <label><input style=\"height: 20px; width:20px; margin: 0 auto; display: inline; float:left;\" type=\"checkbox\"> &emsp;&emsp;</label><i class=\"ion-android-folder\">&emsp;" + fileArray[i].getFileName()+"</i></td>":"<td> <label><input style=\"height: 20px; width:20px; margin: 0 auto; display: inline; float:left;\" type=\"checkbox\">&emsp;&emsp;</label> <i input class=\"glyphicon glyphicon-file\">&emsp;" + fileArray[i].getFileName()+"</i></td>") +
 				"<td>"+fileArray[i].getAttribute()+"</td>"+
 				"<td>"+fileArray[i].getTime()+"</td>"+
 			"</tr>";			
